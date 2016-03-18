71fc77f54fa0ce6103888bc2e7f33fb2148566f1
Lacie Fan Yuxin <lacie@velocloud.net>
2016-03-18 10:49:04 +0800

    Initial Commit

---
+++ HayHay/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HayHay
+//
+//  Created by Lacie on 3/18/16.
+//  Copyright © 2016 Lacie. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
