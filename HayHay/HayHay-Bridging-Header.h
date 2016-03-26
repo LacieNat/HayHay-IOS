887eb86cad738408ffd131acf8aba5d5774bb9c0
Lacie Fan Yuxin <lacie@velocloud.net>
2016-03-26 23:46:19 +0800

    header file

---
+++ HayHay/HayHay-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  HayHay-Bridging-Header.h
+//  HayHay
+//
+//  Created by Lacie on 3/26/16.
+//  Copyright © 2016 Lacie. All rights reserved.
+//
+
+#ifndef HayHay_Bridging_Header_h
+#define HayHay_Bridging_Header_h
+
+#import <CommonCrypto/CommonCrypto.h>
+#endif /* HayHay_Bridging_Header_h */
--- HayHay/LoginController.swift
+++ HayHay/LoginController.swift
@@ -27,16 +27,20 @@ class LoginController: UIViewController {
     @IBAction func signinTap(sender: UIButton) {
         var un:NSString! = email.text;
         var pw:NSString! = password.text;
-        
+       
+        //if username or password is empty throw error
         if(un.isEqualToString("") || pw.isEqualToString("")) {
-            var alertView:UIAlertView = UIAlertView()
-            alertView.title = "Sign in Failed!"
-            alertView.message = "Please enter Username and Password"
-            alertView.delegate = self
-            alertView.addButtonWithTitle("OK")
-            alertView.show()
+            let alert = UIAlertController(title: "Sign In Failed", message:"Please enter both username and password", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default) { _ in })
+            self.presentViewController(alert, animated: true){}
+        }
+        
+        //else process post request
+        else {
+            var post:NSString = "email=\(email)&"
         }
     }
+
 
     /*
     // MARK: - Navigation
@@ -48,4 +52,17 @@ class LoginController: UIViewController {
     }
     */
 
+}
+
+extension String {
+    func sha1() -> String {
+        let data = self.dataUsingEncoding(NSUTF8StringEncoding)!
+        var digest = [UInt8](count:Int(CC_SHA1_DIGEST_LENGTH), repeatedValue: 0)
+        CC_SHA1(data.bytes, CC_LONG(data.length), &digest)
+        let output = NSMutableString(capacity: Int(CC_SHA1_DIGEST_LENGTH))
+        for byte in digest {
+            output.appendFormat("%02x", byte)
+        }
+        return output as String
+    }
 }
