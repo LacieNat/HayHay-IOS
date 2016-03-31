9189421c1142306a902a34ab184701b81cd0acb4
Lacie Fan Yuxin <lacie@velocloud.net>
2016-03-31 21:41:56 +0800

    - solved post bug - workaround over iOS 9 security errors

--- HayHay/LoginController.swift
+++ HayHay/LoginController.swift
@@ -25,8 +25,8 @@ class LoginController: UIViewController {
     }
     
     @IBAction func signinTap(sender: UIButton) {
-        var un:NSString! = email.text;
-        var pw:NSString! = password.text;
+        let un:NSString! = email.text;
+        let pw:NSString! = password.text;
        
         //if username or password is empty throw error
         if(un.isEqualToString("") || pw.isEqualToString("")) {
@@ -41,37 +41,42 @@ class LoginController: UIViewController {
             let datahash = (un as String) + (pw as String)
             let hash = ((nonce as String) + datahash.sha1()).sha1()
             
-            var params = ["email":un as String, "hhash": hash as String, "nonce": nonce as String] as Dictionary<String, String>
-            var request = NSMutableURLRequest(URL: NSURL(string: "https://hayhaytheapp.com:443/user/login")!)
-            var session = NSURLSession.sharedSession()
+            let params = ["email":un as String, "hhash": hash as String, "nonce": nonce as String] as Dictionary<String, String>
+            let request = NSMutableURLRequest(URL: NSURL(string: "https://hayhaytheapp.com:443/user/login")!)
+            let session = NSURLSession.sharedSession()
             
             request.HTTPMethod = "POST"
-            //request.HTTPBody = try? NSJSONSerialization.dataWithJSONObject(params, options: [])
+            request.HTTPBody = try? NSJSONSerialization.dataWithJSONObject(params, options: [])
             request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            //request.setValue("application/json", forHTTPHeaderField: "Accept")
-            request.setBodyContent(params)
-            
 
             var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-                print("Response: \(response)")
                 
                 if(error != nil) {
+                    let alert = UIAlertController(title: "Sign In Failed", message:"Connection Error", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Default) { _ in })
+                    self.presentViewController(alert, animated: true){}
                     print(error!.localizedDescription)
-                    let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
-                    print("Error could not parse JSON: '\(jsonStr)'")
                     return
                 }
-                
-                var json = try? NSJSONSerialization.JSONObjectWithData(data!, options: .MutableLeaves) as? NSDictionary
+                var strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                print(strData)
+                var json = try? NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary
                 // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+                
                 if(json != nil) {
                     // The JSONObjectWithData constructor didn't return an error. But, we should still
                     // check and make sure that json has a value using optional binding.
                     if let parseJSON = json {
-                        // Okay, the parsedJSON is here, let's get the value for 'success' out of it
-                        var success = parseJSON!["success"] as? Int
-                        print("Succes: \(success)")
+                        
+                        var hasErrors = parseJSON!["err"] as? String
+                        
+                        if (hasErrors != nil) {
+                            let alert = UIAlertController(title: "Sign In Failed", message:"Wrong username or password", preferredStyle: .Alert)
+                            alert.addAction(UIAlertAction(title: "OK", style: .Default) { _ in })
+                            self.presentViewController(alert, animated: true){}
+                        }
                     }
+                        
                     else {
                         // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
                         let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
